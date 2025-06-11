7765e91b5ff133c5c26ca169b25b11978ae89313
ArtemAAA <your.email@example.com>
2025-06-11 13:48:27 -0700

    Task 1.6: Create VideoSessionViewSet with CRUD operations

--- backend/tracker_app/views.py
+++ backend/tracker_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import VideoSession
+from .serializers import VideoSessionSerializer
 
 # Create your views here.
+
+class VideoSessionViewSet(ModelViewSet):
+    queryset = VideoSession.objects.all()
+    serializer_class = VideoSessionSerializer
